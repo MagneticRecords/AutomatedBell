5b09cc22442360b4175a3a8aeb859c12c20d99b0
Josh J <Thejoshj@live.com>
2015-07-20 14:23:26 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -7,20 +7,31 @@ class melody_class:
 class controller:
    def __init__(self,initialvelocity):
       self.velocity=initialvelocity
-      self.tone=initialtone
       self.melody = melody_class()
       self.tones = []
       self.rings = []
       self.tones = ['c','d','e','f','g','a','b','C']
       self.rings = ['default','onenote','silence']
    def write_sequence(self,fullmelody):
-      for x in range(len(fullmelody)):
-         self.melody.add_note(fullmelody[x])
+      for x in fullmelody:
+         self.melody.add_note(x)
    def reset(self,inittone):
-      if inittone="default":
+      if inittone=="default":
          self.write_sequence("cdg")
-      if inittone="onenote":
+      if inittone=="onenote":
          self.write_sequence("a")
-      if inittone="silence":
+      if inittone=="silence":
          self.write_sequence("")
+   def testtone(self,tested):
+      for y in range(0,7):
+         if self.tones[y] == tested:
+            #controller: returns 0-7
+            break
+   def play(self):
+      for x in range(self.melody.notes):
+         for y in range(0,7):
+            if self.tones[y] == self.melody.notes[x]:
+               #controller: returns 0-7
+               break
+
          
